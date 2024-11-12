6d80dd17f92ab98890fafc78433e276964e27297
A.F. Adib <a.f.adib36@gmail.com>
2024-11-12 23:56:30 +0600

    globalActors

---
+++ SwiftConcurrency/GlobalActors.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalActors.swift
+//  SwiftConcurrency
+//
+//  Created by A.F. Adib on 12/11/24.
+//
+
+import SwiftUI
+
+struct GlobalActors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlobalActors()
+}
