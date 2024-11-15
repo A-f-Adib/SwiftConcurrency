6e27930f24632ccaf2660dcbd2a1983880a7a02f
A.F. Adib <a.f.adib36@gmail.com>
2024-11-16 01:26:58 +0600

    classObject

--- SwiftConcurrency/StructClassActor.swift
+++ SwiftConcurrency/StructClassActor.swift
@@ -13,7 +13,7 @@ struct MyStruct {
 }
 
 class MyClass {
-    let title: String
+    var title: String
     
     init(title: String) {
         self.title = title
@@ -49,6 +49,32 @@ extension StructClassActor {
                 ObjectB : Starting Title
                 ObjectB title changed
                 ObjectA : Starting Title
+                ObjectB : Second Title
+        */
+    }
+    
+    
+    private func classTest() {
+        
+        let objectA = MyClass(title: "Starting Title")
+        print("ObjectA : ", objectA.title)
+        
+        print("Pass the value of objectA to objectB ")
+        let objectB = objectA
+        print("ObjectB :", objectB.title)
+        
+        objectB.title = "Second Tilte"
+        print("ObjectB title changed")
+        
+        print("ObjectA : ", objectA.title)
+        print("ObjectB : ", objectB.title)
+        
+        /*
+        OutPut:
+                ObjectA : Statring Title
+                ObjectB : Starting Title
+                ObjectB title changed
+                ObjectA : Second Title
                 ObjectB : Second Title
         */
     }
