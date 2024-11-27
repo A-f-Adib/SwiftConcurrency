ea4b219abc5bc841d7c85f6adcd4ae401750808d
A.F. Adib <a.f.adib36@gmail.com>
2024-11-27 23:32:36 +0600

    global actor

--- SwiftConcurrency/GlobalActors.swift
+++ SwiftConcurrency/GlobalActors.swift
@@ -17,7 +17,24 @@ actor NewDataManager {
     }
 }
 
+
+class GlobalActorViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+    let manager = firstGlobalActor.shared
+    
+    @firstGlobalActor func getData() {
+        
+        Task {
+            let data = await manager.getDataFromDatabase()
+            self.dataArray = data
+        }
+    }
+}
+
 struct GlobalActors: View {
+    
+    @StateObject private var viewModel = GlobalActorViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
