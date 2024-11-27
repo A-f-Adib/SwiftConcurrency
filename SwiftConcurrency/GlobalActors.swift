4111917ec8d654e65033ac5a85c78e9cf7924191
A.F. Adib <a.f.adib36@gmail.com>
2024-11-27 23:27:13 +0600

    Global actor

--- SwiftConcurrency/GlobalActors.swift
+++ SwiftConcurrency/GlobalActors.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+@globalActor struct firstGlobalActor {
+    static var shared = NewDataManager()
+}
+
+actor NewDataManager {
+    func getDataFromDatabase() -> [String] {
+        return ["one", "two", "three", "four", "five"]
+    }
+}
+
 struct GlobalActors: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
