17ba38e7404c410b0d95058564dfe7ee8bb72b53
A.F. Adib <a.f.adib36@gmail.com>
2024-12-04 01:54:44 +0600

    searchable modifier

--- SwiftConcurrency/SearchableModifier.swift
+++ SwiftConcurrency/SearchableModifier.swift
@@ -11,20 +11,41 @@ import SwiftUI
 struct Restaurant: Identifiable, Hashable {
     let id : String
     let title: String
+    let cuisine: CuisineOption
 }
 
 
+enum CuisineOption {
+    case american, italian, japanese
+}
 
 
 final class RestaurantManager {
     
+    func getRestaurant() async throws -> [Restaurant] {
+        [
+            Restaurant(id: "1", title: "Burger Club", cuisine: .american),
+            Restaurant(id: "1", title: "Burger Club", cuisine: .american),
+            Restaurant(id: "1", title: "Burger Club", cuisine: .american),
+            Restaurant(id: "1", title: "Burger Club", cuisine: .american)
+        ]
+    }
 }
 
 
 @MainActor
 final class SearchableViewModel: ObservableObject {
     
+    @Published private(set) var allRestaurants: [Restaurant] = []
+    let manager = RestaurantManager()
     
+    func loadRestaurants() async {
+        do {
+            allRestaurants = try await manager.getRestaurant()
+        } catch {
+        print(error)
+        }
+    }
 }
 
 
