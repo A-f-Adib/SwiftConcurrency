d9c6ec6853e29d7dd873192e91c1cbc18e75eaaf
A.F. Adib <a.f.adib36@gmail.com>
2025-01-02 21:26:24 +0600

    select multiple image

--- SwiftConcurrency/PhotoPicker.swift
+++ SwiftConcurrency/PhotoPicker.swift
@@ -11,6 +11,7 @@ import PhotosUI
 @MainActor
 final class PhotoPickerViewModel : ObservableObject {
     
+    //select single image
     @Published private(set) var selectedImage: NSImage? = nil
     @Published var imageSelection : PhotosPickerItem? = nil {
         didSet {
@@ -18,6 +19,17 @@ final class PhotoPickerViewModel : ObservableObject {
         }
     }
     
+    
+    //select multiple image
+    @Published private(set) var selectedImages: [NSImage] = []
+    @Published var imageSelections : [PhotosPickerItem] = [] {
+        didSet {
+            setImages(from: imageSelections)
+        }
+    }
+    
+    
+    //func to select single image
     private func setImage(from selection: PhotosPickerItem?) {
         guard let selection else { return }
         
@@ -31,6 +43,22 @@ final class PhotoPickerViewModel : ObservableObject {
         }
     }
     
+    
+    //func to select multiple image
+    private func setImages(from selections: [PhotosPickerItem]) {
+        
+        Task {
+            var images: [NSImage] = []
+            for selection in selections {
+                if let data = try? await selection.loadTransferable(type: Data.self) {
+                    if let nsImage = NSImage(data: data) {
+                        images.append(nsImage)
+                    }
+                }
+            }
+           selectedImages = images
+        }
+    }
 }
 
 struct PhotoPicker: View {
@@ -41,6 +69,8 @@ struct PhotoPicker: View {
         VStack(spacing: 40) {
             Text("Hello")
             
+            
+            //for select single image
             if let image = viewModel.selectedImage {
                 Image(nsImage: image)
                     .resizable()
@@ -50,6 +80,25 @@ struct PhotoPicker: View {
             }
             
             PhotosPicker(selection: $viewModel.imageSelection) {
+                Text("Open Photo picker")
+                    .foregroundStyle(Color.red)
+            }
+            
+            //for select multiple images
+            if !viewModel.selectedImages.isEmpty {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(viewModel.selectedImages, id: \.self) { image in
+                            Image(nsImage: image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+            }
+            PhotosPicker(selection: $viewModel.imageSelections) {
                 Text("Open Photo picker")
                     .foregroundStyle(Color.red)
             }
